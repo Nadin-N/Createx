b1646e4c155bd14f7d93fe51e8cb058852e80448
Nadin Severin <nadinlikes2018@gmail.com>
2023-09-23 01:16:24 +0300

    add events, certificate, team section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './js/common/header';
 import './js/main_page/benefit';
+import './js/main_page/team-slider';
 import './js/common/footer';
---
+++ src/js/main_page/team-slider.js
@@ -0,0 +1,18 @@
+import Splide from '@splidejs/splide';
+// import { AutoScroll } from '@splidejs/splide-extension-auto-scroll';
+
+const windowWidth = window.screen.width;
+
+new Splide('.splide', {
+  pagination: false,
+  type: 'loop',
+  fixedWidth: '285px',
+  gap: '30px',
+  speed: (number = 500),
+  drag: 'free',
+  focus: `${windowWidth > 1320 ? 1 : 'center'}`,
+  // autoScroll: {
+  //   speed: 0.5,
+  // },
+}).mount();
+// }).mount({ AutoScroll });
